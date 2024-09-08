4c565f9a439ffb49736bb21040a5c02fe0c23bf5
MALIKKHUBAIB1 <khubaib9221@gmail.com>
2024-09-08 16:25:45 +0530

    fix issue

--- src/utils/constant.js
+++ src/utils/constant.js
@@ -19,8 +19,6 @@ export const API_OPTIONS = {
 
 export const IMAGE_URL = "https://image.tmdb.org/t/p/w500/";
 
-export const OPENAI_KEY =
-  "sk-UVPaONJ0yCkf1UmYvoA3o_NZZqofnYgi2tugvccDLIT3BlbkFJwqT4I5sg6vEKF-P_hi_KX2isUZJMQuqQp7wbmgH00A";
 
 export const SUPPORTED_LANGUAGES = [
   {
